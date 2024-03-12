57786913e9160188291e6092e2f9ce1416533432
Nemanja Mitic <mitic.nemanja@hotmail.com>
2024-03-12 23:00:49 +0100

    refactor/Queries for CRUD

--- helpers/queries.php
+++ helpers/queries.php
@@ -2,34 +2,49 @@
 
 function insertFlightInfo(PDO $conn, string $code, string $city, string $country): bool
 {
-    $sql = "INSERT IGNORE INTO filghts(code, city, country) VALUES(:code, :city, :country)";
+    try {
+        $sql = "INSERT IGNORE INTO filghts(code, city, country) VALUES(:code, :city, :country)";
 
-    $statement = $conn->prepare($sql);
-    return $statement->execute([
-        ":code" => $code,
-        ":city" => $city,
-        ":country" => $country
-    ]);
+        $statement = $conn->prepare($sql);
+        return $statement->execute([
+            ":code" => $code,
+            ":city" => $city,
+            ":country" => $country
+        ]);
+    } catch (PDOException $e) {
+        error($e->getMessage());
+    }
+
 }
 
 function updateFlightInfo(PDO $conn, int $id, string $code, string $city, string $country): bool
 {
-    $sql = "UPDATE filghts SET $code=:code, $city=:city, $country=:country WHERE $id=:id";
+    try {
+        $sql = "UPDATE filghts SET $code=:code, $city=:city, $country=:country WHERE $id=:id";
 
-    $statement = $conn->prepare($sql);
-    return $statement->execute([
-        ":id" => $id,
-        ":code" => $code,
-        ":city" => $city,
-        ":country" => $country
-    ]);
+        $statement = $conn->prepare($sql);
+        return $statement->execute([
+            ":id" => $id,
+            ":code" => $code,
+            ":city" => $city,
+            ":country" => $country
+        ]);
+    } catch (PDOException $e){
+        error($e->getMessage());
+    }
+
 }
 
 function deleteFlightInfo(PDO $conn, string $code): bool
 {
-    $sql = "DELETE FROM filghts WHERE $code = :code";
+    try {
+        $sql = "DELETE FROM filghts WHERE $code = :code";
 
-    $statement = $conn->prepare($sql);
-    $statement->bindParam(':code', $code);
-    return $statement->execute();
+        $statement = $conn->prepare($sql);
+        $statement->bindParam(':code', $code);
+        return $statement->execute();
+    } catch (PDOException $e){
+        error($e->getMessage());
+    }
+
 }
