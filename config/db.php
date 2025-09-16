2efc12fd0406da613fed76ec8bd1e8b95533405a
nemanja85 <mitic.nemanja@hotmail.com>
2025-09-16 20:56:56 +0200

    feat: Setup DB connection for SQLite in config file

--- config/db.php
+++ config/db.php
@@ -1,11 +1,17 @@
 <?php
+
+const DB_PATH = '/home/nemanja/Code/personal/FlightCheck/analytics.sqlite';
 function dbConnect(string $db): ?PDO
 {
     try {
-        $dir = 'sqlite://home/nemanja/Code/personal/FlightCheck/analytics.sqlite';
-        return new PDO($dir);
+
+        $dir = "sqlite:$db";
+
+        $pdo = new PDO($dir);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
     } catch (PDOException $exception) {
         dd($exception->getMessage());
     }
 }
-
